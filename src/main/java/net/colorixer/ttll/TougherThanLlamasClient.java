7397e8a3079409b590fb08ed8f19f5f159f3948b
RealColorix <97121912+RealColorix@users.noreply.github.com>
2024-12-20 14:29:23 +0100

    Add files via upload

---
+++ src/main/java/net/colorixer/ttll/TougherThanLlamasClient.java
@@ -0,0 +1,10 @@
+package net.colorixer.ttll;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TougherThanLlamasClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/colorixer/ttll/TougherThanLlamasDataGenerator.java
@@ -0,0 +1,11 @@
+package net.colorixer.ttll;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TougherThanLlamasDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
